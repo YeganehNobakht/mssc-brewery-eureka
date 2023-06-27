d79988a6fadc7d58c849dda2fb9290b14f5bf9d5
yeganehNobakht <m.yeganeh.n1994@gmail.com>
2023-06-27 10:07:34 +0330

    initial add

---
+++ src/main/java/com/bmc/sfg/brewery/eureka/msscbreweryeureka/MsscbreweryeurekaApplication.java
@@ -0,0 +1,15 @@
+package com.bmc.sfg.brewery.eureka.msscbreweryeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MsscbreweryeurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscbreweryeurekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bmc/sfg/brewery/eureka/msscbreweryeureka/MsscbreweryeurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmc.sfg.brewery.eureka.msscbreweryeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscbreweryeurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
